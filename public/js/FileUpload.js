6302f3510570fed3da0202bd1dd7ce8ff26fe071
Harrison Beckerich <hbeckeri@Harrisons-MacBook-Air.local>
2016-04-20 16:15:13 -0400

    did stuff

--- public/js/FileUpload.js
+++ public/js/FileUpload.js
@@ -2,14 +2,23 @@
  * Created by traverclifford on 4/20/16.*/
 
 
-document.getElementById("files").onchange = function () {
+$('#files').change(function(){
     var reader = new FileReader();
 
     reader.onload = function (e) {
-        // get loaded data and render thumbnail.
-        document.getElementById("image").src = e.target.result;
+        $('#image').attr('src', e.target.result);
     };
 
     // read the image file as a data URL.
     reader.readAsDataURL(this.files[0]);
-};
+});
+
+$('#btnSubmit').click(function(){
+    $('form[name=new_upload]').submit(function(){
+        console.log('hree');
+        $.post($(this).attr('action'), $(this).serialize(), function(json) {
+            alert(json);
+        }, 'json');
+        return false;
+    });
+});
