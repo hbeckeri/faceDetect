5831ef37799302f3b697460e114feb19376bd5bc
Harrison Beckerich <hbeckeri@pal-nat186-36-120.itap.purdue.edu>
2016-04-21 12:36:01 -0400

    did stuff

--- public/js/FileUpload.js
+++ public/js/FileUpload.js
@@ -2,11 +2,13 @@
  * Created by traverclifford on 4/20/16.*/
 
 $(function () {
-
+    addText("Upload a photo to guess your age", "");
     $('#files').change(function(){
         var reader = new FileReader();
 
         reader.onload = function (e) {
+            $('.card').empty();
+            addText("Waiting", "");
             $('#image').attr('src', e.target.result);
             $('#myForm').submit();
         };
@@ -19,16 +21,21 @@ $(function () {
     $('#myForm').submit(function() {
         $(this).ajaxSubmit({
             error: function (xhr) {
+                $('.card').empty();
+                addText('Error:', xhr.status);
                 status('Error: ' + xhr.status);
             },
             success: function (response) {
+                $('.card').empty();
+                console.log(response);
                 var array = response.success.imageFaces;
-                var string = "";
+                if (array.length == 0) {
+                    addText('No faces found');
+                }
                 array.forEach(function(data){
-                    string+= "Age Range: " + data.age.ageRange + "\n";
-                    string+= "Gender: " + data.gender.gender+ "\n";
+                    addText('Age Range' ,data.age.ageRange);
+                    addText('Gender', data.gender.gender);
                 });
-                $('#jsonResp').text(string);
             }
         });
         //Very important line, it disable the page refresh.
@@ -39,4 +46,15 @@ $(function () {
        $('#files').click();
     });
 });
+
+function addText(title, string) {
+    var card = $('.card');
+    if (title) {
+        if (string) {
+            card.append('<h3>' + title + ': ' + string + '</h3>');
+        } else {
+            card.append('<h3>' + title + '</h3>');
+        }
+    }
+}
 
